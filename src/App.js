f4db60e2966cb2620ab0412daa37e841773ec1aa
Lukasz Bartkowski <lukasz.bartkowski91@gmail.com>
2022-09-21 13:58:16 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Products from './components/Products/Products';
+import Header from './components/Header/Header';
+import Container from './components/Container/Container';
+import React from 'react';
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      <Container>
+        <Header />
+        <Products />
+      </Container>
+    </React.StrictMode>
+  );
+};
+
+export default App;
