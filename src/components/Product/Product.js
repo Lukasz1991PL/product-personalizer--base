272bda31dab61f8ea866b0f1ee415e5eb3613b34
Lukasz Bartkowski <lukasz.bartkowski91@gmail.com>
2022-09-27 18:33:00 +0200

    a

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,48 +1,81 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import { useState } from 'react';
 
-const Product = (props) => {
-  console.log(props.title);
+const Product = ({ colors, title, sizes, basePrice, name }) => {
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0].name);
+  console.log('sizes', sizes);
+
+  const prepareColorClassName = (color) => {
+    return styles[
+      'color' + color[0].toUpperCase() + color.substr(1).toLowerCase()
+    ];
+  };
 
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img
           className={styles.image}
-          alt='Kodilla shirt'
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`}
+          alt={title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`}
         />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>{props.title}</h2>
-          <span className={styles.price}>{props.basePrice}</span>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>Price:{basePrice}</span>
         </header>
         <form>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li>
-                <button type='button' className={styles.active}>
-                  S
-                </button>
-              </li>
-              <li>
-                <button type='button'>M</button>
-              </li>
-              <li>
-                <button type='button'>L</button>
-              </li>
-              <li>
-                <button type='button'>XL</button>
-              </li>
+              {sizes.map((size) => {
+                console.log(size.name);
+                return (
+                  <li key={size.name}>
+                    <button
+                      onClick={() => setCurrentSize(size.name)}
+                      type='button'
+                      className={clsx(
+                        currentSize === size.name && styles.active
+                      )}
+                    >
+                      {size.name}
+                    </button>
+                  </li>
+                );
+              })}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li>
+              {colors.map((color) => {
+                const foo = `color${color.charAt(0).toUpperCase()}${color.slice(
+                  1
+                )}`;
+
+                return (
+                  <li key={color}>
+                    <button
+                      type='button'
+                      onClick={() => {
+                        setCurrentColor(color);
+                      }}
+                      className={clsx(
+                        styles[foo],
+                        currentColor === color && styles.active
+                      )}
+                    >
+                      aaa
+                    </button>
+                  </li>
+                );
+              })}
+              {/* <li>
                 <button
                   type='button'
                   className={clsx(styles.colorBlack, styles.active)}
@@ -53,10 +86,21 @@ const Product = (props) => {
               </li>
               <li>
                 <button type='button' className={clsx(styles.colorWhite)} />
-              </li>
+              </li> */}
             </ul>
           </div>
-          <Button className={styles.button}>
+          <Button
+            onClick={() =>
+              console.log(
+                'Summary',
+                '=======',
+                'Name;'({ title }),
+                'Price',
+                'Color'
+              )
+            }
+            className={styles.button}
+          >
             <span className='fa fa-shopping-cart' />
           </Button>
         </form>
